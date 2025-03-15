b41bb682fb5439251d1dcb9bbecd9960c38f3bfa
Arnanz <pashamaulana14@gmail.com>
2025-03-15 13:59:44 +0700

    Praktikum 4

--- PWL_POS/routes/web.php
+++ PWL_POS/routes/web.php
@@ -1,21 +1,9 @@
 <?php
-
+use App\Http\Controllers\LevelController;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\SalesController;
+Route::get('/', function () {
+    return view('welcome');
+}); 
 
-Route::get('/', [HomeController::class, 'index']);
-
-Route::prefix('/category')->group(function () {
-    Route::get('/food-beverage', [ProductController::class, 'foodBeverage']);
-    Route::get('/beauty-health', [ProductController::class, 'beautyHealth']);
-    Route::get('/home-care', [ProductController::class, 'homeCare']);
-    Route::get('/baby-kid', [ProductController::class, 'babyKid']);
-});
-
-Route::get('/user/profile', [UserController::class, 'profile']);
-
-Route::get('/sales', [SalesController::class, 'index']);
+Route::get('/level', [LevelController::class, 'index']);
