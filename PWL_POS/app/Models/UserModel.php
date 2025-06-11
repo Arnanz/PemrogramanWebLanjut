3df1d02adb94b4847e24d7227233e257c0b5f0f2
Arnanz <pashamaulana14@gmail.com>
2025-06-11 09:43:24 +0700

    Praktikum 1 JS 10

--- PWL_POS/app/Models/UserModel.php
+++ PWL_POS/app/Models/UserModel.php
@@ -1,27 +1,64 @@
 <?php
 
 namespace App\Models;
-use App\Models\LevelModel;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
-class UserModel extends Authenticatable
+class UserModel extends Authenticatable implements JWTSubject
 {
-    use HasFactory;
-    protected $table ='m_user';
-    protected $primaryKey = 'user_id';
 
-    protected $fillable = ['level_id', 'username', 'nama', 'password'];
-    protected $hidden = ['password'];
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
+    use HasFactory;
+
+    protected $table = 'm_user';  // Mendefinisikan nama tabel yang digunakan oleh model ini
+    protected $primaryKey = 'user_id';  // Mendefinisikan primary key dari tabel yang digunakan
+
+    protected $fillable = [
+        'level_id',
+        'username',
+        'nama',
+        'password',
+        'foto_profil',
+        'image', //tambahan
+        'created_at',
+        'updated_at'
+    ];
+
+    protected $hidden = ['password']; // jangan ditampilkan saat select
+
     protected $casts = [
-        'password' => 'hashed'];
+        'password' => 'hashed',
+    ]; // casting agar password dienkripsi otomatis
+
+    //relasi ke tabel level
     public function level(): BelongsTo
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
     }
-        //mendapatkan nama role
+
+    //image
+    protected function image(): Attribute
+    {  // Missing opening brace here
+        return Attribute::make(
+            get: fn($image) => asset('storage/posts/' . $image),
+        );
+    }
+
+    //mendapatkan nama role
     public function getRoleName(): string
     {
         return $this->level->level_nama;
@@ -31,5 +68,11 @@ class UserModel extends Authenticatable
     public function hasRole(string $role): bool
     {
         return $this->level->level_nama == $role;
+    }
+
+    //mendapatkan kode role
+    public function getRole(): string
+    {
+        return $this->level->level_code;
     }
 }
--- PWL_POS/routes/api.php
+++ PWL_POS/routes/api.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\Api\LevelController;
+use App\Http\Controllers\Api\RegisterController;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\KategoriController;
+use App\Http\Controllers\Api\BarangController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +19,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
