faba9b6d0eac4530d9bb89b56c642521a9a3b177
Arnanz <pashamaulana14@gmail.com>
2025-06-11 11:14:17 +0700

    Praktikum 4 JS 10

--- PWL_POS/routes/api.php
+++ PWL_POS/routes/api.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Api\BarangController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -25,3 +26,9 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
+//Level Route
+Route::get('/levels', [LevelController::class, 'index']);
+Route::post('/levels', [LevelController::class, 'store']);
+Route::get('/levels/{level}', [LevelController::class, 'show']);
+Route::put('/levels/{level}', [LevelController::class, 'update']);
+Route::delete('/levels/{level}', [LevelController::class, 'destroy']);
